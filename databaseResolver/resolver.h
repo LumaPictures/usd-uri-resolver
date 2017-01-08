d6f33f29ee371369c23cc193e6a135dfb203d586
Pal Mezei <sirpalee@gmail.com>
2017-01-08 19:20:07 +1100

    Making the resolver inherit from the default resolver.

--- databaseResolver/resolver.h
+++ databaseResolver/resolver.h
@@ -21,10 +21,10 @@
 // KIND, either express or implied. See the Apache License for the specific
 // language governing permissions and limitations under the Apache License.
 //
-#ifndef AR_DEFAULT_RESOLVER_H
-#define AR_DEFAULT_RESOLVER_H
+#ifndef AR_DATABASE_RESOLVER_H
+#define AR_DATABASE_RESOLVER_H
 
-#include "pxr/usd/ar/resolver.h"
+#include "pxr/usd/ar/defaultResolver.h"
 
 #include <tbb/enumerable_thread_specific.h>
 
@@ -39,101 +39,11 @@
 /// that does not exists on the dist.
 ///
 class databaseResolver
-    : public ArResolver
+    : public ArDefaultResolver
 {
 public:
     databaseResolver();
     virtual ~databaseResolver();
-
-    // ArResolver overrides
-    virtual void ConfigureResolverForAsset(const std::string& path);
-
-    virtual std::string AnchorRelativePath(
-        const std::string& anchorPath,
-        const std::string& path);
-
-    virtual bool IsRelativePath(const std::string& path);
-    virtual bool IsRepositoryPath(const std::string& path);
-    virtual bool IsSearchPath(const std::string& path);
-
-    virtual std::string GetExtension(const std::string& path);
-
-    virtual std::string ComputeNormalizedPath(const std::string& path);
-
-    virtual std::string ComputeRepositoryPath(const std::string& path);
-
-    virtual std::string ComputeLocalPath(const std::string& path);
-
-    virtual std::string Resolve(const std::string& path);
-
-    virtual std::string ResolveWithAssetInfo(
-        const std::string& path,
-        ArAssetInfo* assetInfo);
-
-    virtual void UpdateAssetInfo(
-        const std::string& identifier,
-        const std::string& filePath,
-        const std::string& fileVersion,
-        ArAssetInfo* assetInfo);
-
-    virtual VtValue GetModificationTimestamp(
-        const std::string& path,
-        const std::string& resolvedPath);
-
-    virtual bool FetchToLocalResolvedPath(
-        const std::string& path,
-        const std::string& resolvedPath);
-
-    virtual bool CanWriteLayerToPath(
-        const std::string& path,
-        std::string* whyNot);
-
-    virtual bool CanCreateNewLayerWithIdentifier(
-        const std::string& identifier,
-        std::string* whyNot);
-
-    virtual ArResolverContext CreateDefaultContext();
-
-    virtual ArResolverContext CreateDefaultContextForAsset(
-        const std::string& filePath);
-
-    virtual ArResolverContext CreateDefaultContextForDirectory(
-        const std::string& fileDirectory);
-
-    virtual void RefreshContext(const ArResolverContext& context);
-
-    virtual ArResolverContext GetCurrentContext();
-
-protected:
-    virtual void _BeginCacheScope(
-        VtValue* cacheScopeData);
-
-    virtual void _EndCacheScope(
-        VtValue* cacheScopeData);
-
-    virtual void _BindContext(
-        const ArResolverContext& context,
-        VtValue* bindingData);
-
-    virtual void _UnbindContext(
-        const ArResolverContext& context,
-        VtValue* bindingData);
-
-private:
-    struct _Cache;
-    using _CachePtr = std::shared_ptr<_Cache>;
-    _CachePtr _GetCurrentCache();
-
-    std::string _ResolveNoCache(const std::string& path);
-
-private:
-    std::vector<std::string> _searchPath;
-
-    using _CachePtrStack = std::vector<_CachePtr>;
-    using _PerThreadCachePtrStack =
-    tbb::enumerable_thread_specific<_CachePtrStack>;
-
-    _PerThreadCachePtrStack _threadCacheStack;
 };
 
 #endif // AR_DEFAULT_RESOLVER_H
